bb65ecfb9dec419cd0b81a73ec4a8ed67f3f555c
erickovisck <141879391+erickovisck@users.noreply.github.com>
2023-09-05 13:07:42 -0300

    Update insert.php

--- insert.php
+++ insert.php
@@ -7,6 +7,7 @@
 </head>
 <body>
     <form action="" method="post">
+   
         <h1>nome</h1>
         <input type="text" name="nome">
         <h1>autor</h1>
@@ -19,15 +20,31 @@
 </html>
 <?php
 require_once "conexao/conexao.php";
-$nome=$_POST['nome'];
-$autor=$_POST['autor'];
-$preco=$_POST['preco'];
 
-$sql="INSERT INTO livros(nome_livro, nome_autor, preco) VALUES('$nome', '$autor', '$preco')";
-$resultado=$conexao->query($sql);
-if($resultado){
+$nome = $_POST["nome"];
+$autor = $_POST["autor"];
+$preco = $_POST["preco"];
+
+$sql = "INSERT INTO livros (nome_livro, nome_autor, preco) VALUES (?, ?, ?)";
+
+$erro = $conexao->prepare($sql);
+$erro->bind_param("sss", $nome, $autor, $preco);
+
+if ($erro->execute()) {
+    echo "Cadastrado";
+} else {
+    echo "Erro: " . $erro->error;
+}
+
+$erro->close();
+$conexao->close();
+
+
+/* if(inserirlivro($nome,$autor,$preco,$conexao)){
     echo"cadastrado";
 }else{
     echo "erro";
-}
+} */
+
+
 ?>
