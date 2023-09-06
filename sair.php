8e0ecdf4db245e7fac1ea5addfa93de2c84fc312
erickovisck <141879391+erickovisck@users.noreply.github.com>
2023-09-05 22:41:42 -0300

    Update sair.php

--- sair.php
+++ sair.php
@@ -4,9 +4,10 @@ session_start();
 session_unset();
 session_destroy();
 for($i=0; $i<3; $i++){
-    echo"Você saiu";
+    echo"<script language='javascript' type='text/javascript'>alert('Você saiu')
+    ;window.location.href='index.php'</script>";;
     $i++;
 }
-header("Location: index.php");
+
 exit();
 ?>
