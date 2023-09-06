1bb0dadd68e824349fb2db71ea4cc71534d31519
erickovisck <141879391+erickovisck@users.noreply.github.com>
2023-09-06 07:48:49 -0300

    Update perfil.php

---
+++ perfil.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Perfil</title>
+    <?php 
+require_once "conexao/conexao.php";
+session_start();
+$usuario=$_SESSION["usuario"];
+
+    ?>
+</head>
+<body>
+    
+</body>
+</html>
