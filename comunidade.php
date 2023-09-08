ba0285da20cb386fb3a6a674b3cd7c2b5a97ddab
erickovisck <141879391+erickovisck@users.noreply.github.com>
2023-09-08 16:51:52 -0300

    Update comunidade.php

--- comunidade.php
+++ comunidade.php
@@ -1,71 +1,109 @@
+<?php
+    session_start();
+
+    require_once "conexao/conexao.php";
+
+    $usuario = $_SESSION["usuario"];
+    
+    echo $usuario["nome_usuario"]; 
+
+
+    if(is_null($usuario["email"])){
+        session_unset();
+        session_destroy();
+        header("Location: index.php");
+        exit();
+    }
+    ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="assets\css/estilo.css">
+    <link rel="stylesheet" href="assets/css/estilo.css">
 
 </head>
+
 <body>
-    
-<script src="script.js"></script>
-<div class="cabecalho">  
-<button id="toggleButton">Abrir Menu</button>
 
 
+    <header class="cabecalho">
+        <div class="btnMenu">
+            <button id="toggleButton">Abrir Menu</button>
+        </div>
+        
+                <a href="carrinho.php"> carrinho</a>
 
-    <div id="sidebar">
-    <button id="toggleButton2">Fechar Menu</button>
-        <ul> 
-<h2>
-            <img src="">
-            Olá <?php
-            session_start();
+         
+        <div class="cabecalhoMenu">
+            <div class="headerMenu">
+                <div class="closeMenu">
+                    <button id="toggleButton2">Fechar Menu</button>
+                </div>
+                <div class="headerMenuTitle">
+                    <h2>
+                        <img src="">
+                        Olá
+                    </h2>
+                </div>
+            </div>
+            <div class="contentMenu">
+                <ul>
+                    <li><a href="inicial.php">Inicial</a></li>
+                    <li><a href="perfil.php">Perfil</a></li>
+                    <li><a href="ajuda.php">Ajuda</a></li>
+                    <li><a href="configuracoes.php">Configurações</a></li>
+                    <li><a href="amigos.php">Amigos</a></li>
+                    <li><a href="autores.php">Autores</a></li>
+                    <li><a href="sobre_nos.php">Sobre nós</a></li>
+                    <li><a href="sair.php">Sair</a></li>
+                </ul>
+            </div>
+            <input type="text" name="pesquisa" id="pesquisa">
+            <img url="imagens/teste.png">
+        </div>
+    </header>
+    <main class="principal">
+    <form action="" method="post">
+<input type="text" name="mensagem">
+<input type="submit" value="enviar">
 
-            require_once "conexao/conexao.php";
-
-            $usuario = $_SESSION['usuario'];
-
-            $sql = "SELECT nome_usuario FROM usuario WHERE email = '". $usuario['email']."'";
-           
-            $resultado = $conexao->query($sql);
-            $linha = $resultado->fetch_assoc();
-            $nomeUsuario = $linha['nome_usuario'];
-
-            echo $nomeUsuario;
-
-            $conexao->close();
-?> 
-</h2>
-<li><a href="perfil.php">Perfil</a></li>
-            <li><a href="ajuda.php">Ajuda</a></li>
-            <li><a href="configuracoes.php">Configurações</a></li>
-            <li><a href="amigos.php">Amigos</a></li>
-            <li><a href="autores.php">Autores</a></li>
-            <li><a href="sobre_nos.php">Sobre nós</a></li>
-            <li><a href="sair.php">Sair</a></li>
-        </ul>
-     
-</div>
-<input type="text" name="pesquisa" id="pesquisa"> 
-<img url="imagens/teste.png">
-</div>
-<div class="principal">
-<div class="banners">
-<a href="banners.php">Banners</a>
-</div>
-<div class="comunidade">  
-<a href=""> Comunidade</a>
-</div>
-<div class="itens">
-    <a href="">Itens</a>
-</div>
-<div class="sugestao">
-    <a href=""> Sugestão de livros</a>
-    <img href="imagens/carrinho.png">
-</div>
-</div>
+</form> 
+  <?php
+  $id=$usuario["id_usuario"];
+  $mensagem=$_POST["mensagem"];
+  $sql="INSERT INTO chat_geral (mensagens, id_usuario, nome_usuario) VALUES
+   ('$mensagem','".$usuario['id_usuario']."', '".$usuario['nome_usuario']."')";
+   $resultado=$conexao->query($sql);
+   if($resultado){
+   echo "mensagem enviada <br>";
+   }else{
+       echo "erro ao enviar";
+   }
+   $sql = "SELECT * FROM chat_geral";
+   $resultado = $conexao->query($sql);
    
+   if ($resultado) {
+       while ($dados = mysqli_fetch_array($resultado)) {
+           echo "Nome do Usuário: " . $dados["nome_usuario"] . "<br>";
+           echo "Mensagem: " . $dados["mensagens"] . "<br> <br>";
+
+       }
+   
+       // Feche o resultado após o uso
+       mysqli_free_result($resultado);
+   } else {
+       // Lida com erros de consulta, se houverem
+       echo "Erro na consulta: " . mysqli_error($conexao);
+   }
+   
+   $conexao->close();
+   
+  ?>
+    </main>
+    <script src="script.js"></script>
 </body>
+
 </html>
