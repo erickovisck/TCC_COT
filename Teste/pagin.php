f82b2dcead4b657606fb6a9acf9f17114e969d72
erickovisck <141879391+erickovisck@users.noreply.github.com>
2023-09-27 09:50:50 -0300

    Add files via upload

---
+++ Teste/pagin.php
@@ -0,0 +1,161 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="css.css" rel="stylesheet";>
+</head>
+<body>
+<header class="header">
+  <div class="logo">
+    Logo
+  </div>
+  <nav class="main-nav">
+    <ul>
+      <li>Home</li>
+      <li>Icon</li>
+      <li>Icon 2</li>      
+    </ul>
+  </nav>
+</header>
+<main>
+  <section class="news-feed">
+    <article class="post">
+      <div class="post__header">
+        <img class="avatar" src="http://via.placeholder.com/50x50" />
+        <div class="post__info">
+          <a href="#">User Name</a>
+          <span>Just Now</span>
+        </div>        
+      </div>
+      <div class="post__content">
+        <img src="http://via.placeholder.com/490x400" />
+      </div>
+      <div class="post__engage">
+        <a href="#">Like</a>
+        <a href="#">Comment</a>
+        <a href="#">Share</a>
+      </div>
+      <div class="comment">
+        <img class="avatar" src="http://via.placeholder.com/50x50" />
+        <input type="text" placeholder="Write a comment..">
+      </div>
+    </article>
+		<article class="post">
+      <div class="post__header">
+        <img class="avatar" src="http://via.placeholder.com/50x50" />
+        <div class="post__info">
+          <a href="#">User Name</a>
+          <span>Just Now</span>
+        </div>        
+      </div>
+      <div class="post__content">
+        <img src="http://via.placeholder.com/490x400" />
+      </div>
+      <div class="post__engage">
+        <a href="#">Like</a>
+        <a href="#">Comment</a>
+        <a href="#">Share</a>
+      </div>
+      <div class="comment">
+        <img class="avatar" src="http://via.placeholder.com/50x50" />
+        <input type="text" placeholder="Write a comment..">
+      </div>
+    </article>
+		<article class="post">
+      <div class="post__header">
+        <img class="avatar" src="http://via.placeholder.com/50x50" />
+        <div class="post__info">
+          <a href="#">User Name</a>
+          <span>Just Now</span>
+        </div>        
+      </div>
+      <div class="post__content">
+        <img src="http://via.placeholder.com/490x400" />
+      </div>
+      <div class="post__engage">
+        <a href="#">Like</a>
+        <a href="#">Comment</a>
+        <a href="#">Share</a>
+      </div>
+      <div class="comment">
+        <img class="avatar" src="http://via.placeholder.com/50x50" />
+        <input type="text" placeholder="Write a comment..">
+      </div>
+    </article>
+		<article class="post">
+      <div class="post__header">
+        <img class="avatar" src="http://via.placeholder.com/50x50" />
+        <div class="post__info">
+          <a href="#">User Name</a>
+          <span>Just Now</span>
+        </div>        
+      </div>
+      <div class="post__content">
+        <img src="http://via.placeholder.com/490x400" />
+      </div>
+      <div class="post__engage">
+        <a href="#">Like</a>
+        <a href="#">Comment</a>
+        <a href="#">Share</a>
+      </div>
+      <div class="comment">
+        <img class="avatar" src="http://via.placeholder.com/50x50" />
+        <input type="text" placeholder="Write a comment..">
+      </div>
+    </article>
+		<article class="post">
+      <div class="post__header">
+        <img class="avatar" src="http://via.placeholder.com/50x50" />
+        <div class="post__info">
+          <a href="#">User Name</a>
+          <span>Just Now</span>
+        </div>        
+      </div>
+      <div class="post__content">
+        <img src="http://via.placeholder.com/490x400" />
+      </div>
+      <div class="post__engage">
+        <a href="#">Like</a>
+        <a href="#">Comment</a>
+        <a href="#">Share</a>
+      </div>
+      <div class="comment">
+        <img class="avatar" src="http://via.placeholder.com/50x50" />
+        <input type="text" placeholder="Write a comment..">
+      </div>
+    </article>
+		<article class="post">
+      <div class="post__header">
+        <img class="avatar" src="http://via.placeholder.com/50x50" />
+        <div class="post__info">
+          <a href="#">User Name</a>
+          <span>Just Now</span>
+        </div>        
+      </div>
+      <div class="post__content">
+        <img src="http://via.placeholder.com/490x400" />
+      </div>
+      <div class="post__engage">
+        <a href="#">Like</a>
+        <a href="#">Comment</a>
+        <a href="#">Share</a>
+      </div>
+      <div class="comment">
+        <img class="avatar" src="http://via.placeholder.com/50x50" />
+        <input type="text" placeholder="Write a comment..">
+      </div>
+    </article>
+  </section>
+  <aside class="side-nav">
+    
+  </aside>
+  <aside class="services">
+    
+  </aside>
+</main>
+  
+</body>
+</html>
