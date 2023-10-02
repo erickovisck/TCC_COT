d3a57d4afe66ded972ce4474eea1759ca816d1d6
erickovisck <141879391+erickovisck@users.noreply.github.com>
2023-10-02 12:21:11 -0300

    Update carrinho.php

--- carrinho.php
+++ carrinho.php
@@ -43,14 +43,38 @@ if (isset($_POST["selecionado"])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="assets/css/estilo.css">
+    <link rel="stylesheet" href="assets/css/menu.css">
 
 </head>
 
 <body>
-    <div class="cabecalho">
-        <div class="btnMenu">
-            <button id="toggleButton"><img src="imagens/menu_FILL0_wght400_GRAD0_opsz24.png"></button>
-        </div>
+<div class="cabecalho">
+        <nav role="navigation">
+            <div id="menuToggle">
+
+                <input type="checkbox" />
+                <span></span>
+                <span></span>
+                <span></span>
+             
+                   
+
+                    <ul id="menu">
+                        <h2>Usuário: <?= $usuario['nome_usuario']?> </h2>
+                        <li><a href="perfil.php">Perfil</a></li>
+                        <li><a href="ajuda.php">Ajuda</a></li>
+                        <li><a href="configuracoes.php">Configurações</a></li>
+                        <li><a href="amigos.php">Amigos</a></li>
+                        <li><a href="autores.php">Autores</a></li>
+                        <li><a href="sobre_nos.php">Sobre nós</a></li>
+                        <li><a href="sair.php">Sair</a></li>
+                    </ul>
+
+            
+            </div>
+        </nav>
+
+
         <form action="" method="post" class="botpesquisa">
             <input type="text" name="pesquisa" id="pesquisa">
             <button type="submit" name="pesquisar" id="iconpesquisa">
@@ -63,33 +87,6 @@ if (isset($_POST["selecionado"])) {
         </form>
 
     </div>
-    <!--     menu lateral -->
-    <div class="cabecalhoMenu">
-        <div class="headerMenu">
-            <div class="closeMenu">
-                <button id="toggleButton2"><img src="imagens/close_FILL0_wght400_GRAD0_opsz24">
-                </button>
-            </div>
-            <div class="headerMenuTitle">
-
-            </div>
-        </div>
-        <div class="contentMenu">
-            <ul>
-                <h2>Usuário: <?= $usuario['nome_usuario']?> </h2>
-                <li><a href="inicial.php">Inicial</a></li>
-                <li><a href="perfil.php">Perfil</a></li>
-                <li><a href="ajuda.php">Ajuda</a></li>
-                <li><a href="configuracoes.php">Configurações</a></li>
-                <li><a href="amigos.php">Amigos</a></li>
-                <li><a href="autores.php">Autores</a></li>
-                <li><a href="sobre_nos.php">Sobre nós</a></li>
-                <li><a href="sair.php">Sair</a></li>
-            </ul>
-        </div>
-
-    </div>
-    <!--  fim menu -->
     <script src="script.js"></script>
     
     <h1>Meu carrinho</h1>
