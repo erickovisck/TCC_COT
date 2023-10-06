162dfba333a880dd25152841be9e27b2c6c21e2e
erickovisck <141879391+erickovisck@users.noreply.github.com>
2023-10-05 22:38:06 -0300

    Update botaolike.js

--- js/botaolike.js
+++ js/botaolike.js
@@ -1,45 +1,43 @@
+// ...
 
-document.querySelectorAll('.like_button').forEach((button) => {
-    button.addEventListener('click', () => {
-    
-      const messageId = button.parentNode.getAttribute('data-id');
-      const cont_likeElement = button.parentNode.querySelector('.likes');
-      let cont_like = parseInt(cont_likeElement.innerHTML);
+document.addEventListener("DOMContentLoaded", function () {
+    document.querySelectorAll('.like_button').forEach((button) => {
+        button.addEventListener('click', () => {
+            const messageId = button.parentNode.getAttribute('data-id');
+            const cont_likeElement = button.parentNode.querySelector('.likes');
+            let cont_like = parseInt(cont_likeElement.innerHTML);
 
-      // Verifique se o usuário já deu "like"
-      const hasLiked = button.classList.contains('liked');
+            // Verifique se o usuário já deu "like"
+            const hasLiked = button.classList.contains('liked');
 
-      if (hasLiked) {
-          // Remove o "like"
-          cont_like--;
-          button.classList.remove('liked');
-      } else {
-          // Adiciona o "like"
-          cont_like++;
-          button.classList.add('liked');
-      }
+            if (hasLiked) {
+                // Remove o "like"
+                cont_like--;
+                button.classList.remove('liked');
+            } else {
+                // Adiciona o "like"
+                cont_like++;
+                button.classList.add('liked');
+            }
 
-      // Enviar cont_like para o PHP usando AJAX
-      const xhr = new XMLHttpRequest();
-      xhr.open('POST', '    .php');
-      xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-      xhr.onload = () => {
-          if (xhr.status === 200) {
-              // Atualize o valor de cont_like no elemento HTML após a confirmação do servidor
-              cont_likeElement.innerHTML = cont_like;
-          } else {
-              // Lida com erros, se houverem
-              console.error('Erro na solicitação AJAX');
-          }
-      };
+            // Enviar cont_like para o PHP usando AJAX
+            const xhr = new XMLHttpRequest();
+            xhr.open('POST', 'atualizar_likes.php');
+            xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+            xhr.onload = () => {
+                if (xhr.status === 200) {
+                    // Atualize o valor de cont_like no elemento HTML após a confirmação do servidor
+                    cont_likeElement.innerHTML = cont_like;
+                } else {
+                    // Lida com erros, se houverem
+                    console.error('Erro na solicitação AJAX');
+                }
+            };
 
-      const formData = new FormData();
-formData.append('id_mensagem', messageId);
-formData.append('cont_like', cont_like);
+            // Crie uma string de dados a serem enviados
+            const data = `id_mensagem=${messageId}&cont_like=${cont_like}`;
 
-xhr.send(formData);
-
-  });
+            xhr.send(data);
+        });
+    });
 });
-
-
