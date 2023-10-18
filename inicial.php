17a8cf7274d63e179357a4cc38f8fa90d871c662
erickovisck <141879391+erickovisck@users.noreply.github.com>
2023-10-17 23:00:58 -0300

    Update inicial.php

--- inicial.php
+++ inicial.php
@@ -33,7 +33,8 @@ $conexao->close();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="author" content="colorlib.com">
 
-    <title>Document</title>
+ <title>Inical</title>
+    <link rel="shortcut icon" href="imagens/logo_empresa.jpg">
     <link rel="stylesheet" href="assets/css/estilo.css">
    
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
