ba3cfcfeb3cdbf98ea4a5a71fc77c6b92368cda4
erickovisck <141879391+erickovisck@users.noreply.github.com>
2023-10-17 22:56:00 -0300

    Update alterar_senha.php

--- alterar_senha.php
+++ alterar_senha.php
@@ -6,7 +6,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Alterar senha</title>
+     <title>Alterar senha</title>
+    <link rel="shortcut icon" href="imagens/logo_empresa.jpg">
     <link rel="stylesheet" href="assets/css/cadastro.css">
 
 </head>
