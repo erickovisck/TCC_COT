47cc44580c2f51bad636f261c455a638fa0095f6
GPereira10 <143447877+GPereira10@users.noreply.github.com>
2023-10-23 22:12:44 -0300

    Update perfil.php

--- perfil.php
+++ perfil.php
@@ -67,6 +67,7 @@ if($resultado){
     <title>Perfil</title>
     <link rel="shortcut icon" href="imagens/logo_empresa.jpg">
     <link rel="stylesheet" href="assets/css/estilo.css">
+ <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
 </head>
 <body>
 <div class="cabecalho">
