c8ee7d833d5b35e27b5dcecea977387e0fc59c88
camilavsyu <147069206+camilavsyu@users.noreply.github.com>
2023-10-25 08:16:32 -0300

    Update mensagem.php

--- mensagem.php
+++ mensagem.php
@@ -73,7 +73,6 @@ if ($resultado) {
                     <li><a href="inicial.php">Inicial</a></li>
                     <li><a href="perfil.php">Perfil</a></li>
                     <li><a href="ajuda.php">Ajuda</a></li>
-                    <li><a href="configuracoes.php">Configurações</a></li>
                     <li><a href="amigos.php">Amigos</a></li>
                     <li><a href="autores.php">Autores</a></li>
                     <li><a href="sobre_nos.php">Sobre nós</a></li>
