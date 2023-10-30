9f7e542b30454c65e08eb33375b6e80b0ca8088d
D4nntas <143447163+D4nntas@users.noreply.github.com>
2023-10-30 19:50:55 -0300

    Add files via upload

--- Nova pasta/script.js
+++ Nova pasta/script.js
@@ -1,20 +1,46 @@
-var carouseWidth = $('.carousel-inner')[0].scrolWidth;
-var cardWidth = $('.carousel-item').width();
+if (window.matchMedia("(min-width:576px)").matches) {
+  var $carousel = $('#carouselExample');
+  var cardWidth = $('.carousel-item').width();
+  var scrollPosition = 0;
 
-var scrollPosition = 0;
+  // Função para copiar os primeiros slides e anexá-los ao final
+  function copyAndAppendSlides() {
+    var $carouselInner = $carousel.find('.carousel-inner');
+    var $firstSlides = $carouselInner.find('.carousel-item:lt(4)').clone();
+    $carouselInner.append($firstSlides);
+  }
 
-$('.carousel-control-next').on('click', function(){
-    if(scrollPosition < (carouselWidth - (cardWidth * 4))){
-    console.log('next');
-    scrollPosition = scrollPosition + cardWidth;
-    $('.carousel-inner').animate({scrollLeft: scrollPosition}, 600);
+  copyAndAppendSlides();
+
+  $('.carousel-control-next').on('click', function () {
+    var carouselWidth = $('.carousel-inner')[0].scrollWidth;
+    if (scrollPosition < (carouselWidth - (cardWidth * 4))) {
+      console.log('next');
+      scrollPosition = scrollPosition + cardWidth;
+      $('.carousel-inner').animate({ scrollLeft: scrollPosition }, 600);
+    } else {
+      // Se você chegou ao fim, volte para o início
+      scrollPosition = 0;
+      $('.carousel-inner').animate({ scrollLeft: scrollPosition }, 600, function () {
+        copyAndAppendSlides();
+      });
+    }
+  });
+
+  $('.carousel-control-prev').on('click', function () {
+    if (scrollPosition > 0) {
+      console.log('prev');
+      scrollPosition = scrollPosition - cardWidth;
+      $('.carousel-inner').animate({ scrollLeft: scrollPosition }, 600);
+    } else {
+      // Se você está no início, vá para o fim
+      var carouselWidth = $('.carousel-inner')[0].scrollWidth;
+      scrollPosition = carouselWidth - (cardWidth * 4);
+      $('.carousel-inner').animate({ scrollLeft: scrollPosition }, 600, function () {
+        copyAndAppendSlides();
+      });
+    }
+  });
+} else {
+  // Comportamento para telas menores (se necessário)
 }
-});
-
-$('.carousel-control-prev').on('click', function(){
-    if(scrollPosition > 0){
-    console.log('prev');
-    scrollPosition = scrollPosition - cardWidth;
-    $('.carousel-inner').animate({scrollLeft: scrollPosition}, 600);
-}
-});
