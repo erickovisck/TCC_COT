78ffcff27b924a9d1dad1fe2fcdb0caef8032bcb
GPereira10 <143447877+GPereira10@users.noreply.github.com>
2023-10-30 12:13:01 -0300

    Update autores.php

---
+++ autores.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+    <title>prototipo</title>
+</head>
+<style>
+body{
+    background-color:#191825;
+    color:white;
+}
+img{
+    width:300px;
+    height: 300px;
+}
+.prototipo {
+    grid-area: principal;
+display: flex;
+flex-direction: column;
+}
+.pro{
+    text-align:left;
+    margin-top
+}
+h1{
+    font-weight: bold;
+    font-size: 50px;
+    margin-top:20%
+}
+p{
+    font-size: 35px;
+}
+    </style>
+<body>
+<main class="prototipo">
+<div class="container text-center" style="margin-top:20%; overflow-y:hidden;  overflow-x:hidden;">
+<div class="row">
+<div class="col">
+    <h1>OPS!!Tenha Calma</h1>
+    <p>Essa página está em desenvolvimento.</p>
+</div>
+    <div class="col">
+    <img src="imagens/prototipo.png">
+</div>
+</div>
+</div>
+</main>
+</body>
+</html>
