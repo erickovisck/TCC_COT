07b468455eaa7ac0b6fc030921e32777e29af089
erickovisck <141879391+erickovisck@users.noreply.github.com>
2023-10-30 12:13:20 -0300

    Update Login.php

--- Login.php
+++ Login.php
@@ -3,10 +3,13 @@
 
 <head>
     <meta charset="UTF-8">
+    
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200;300;400;500;600;700&display=swap"
         rel="stylesheet">
+        <link rel="shortcut icon" href="imagens/logo_empresa.jpg">
+
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="assets/css/cadastro.css">
