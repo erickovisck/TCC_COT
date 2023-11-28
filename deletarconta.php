10cbdcd36cb5ba5d1aef28eb51971dfd6a26a562
GPereira10 <143447877+GPereira10@users.noreply.github.com>
2023-11-28 11:10:56 -0300

    Update deletarconta.php

--- deletarconta.php
+++ deletarconta.php
@@ -1,54 +1,116 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
+<meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="colorlib.com">
    <title>Deletar conta</title>
-    <link rel="shortcut icon" href="imagens/logo_projeto2.png">
-    <link rel="stylesheet" href="assets/css/cadastro.css">
+   <link rel="shortcut icon" href="imagens/logo_projeto2.png">
+    <link rel="stylesheet" href="assets/css/estilo.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
 
 </head>
+
 <body>
+<div class="cabecalho">
+        <nav role="navigation">
+            <div id="menuToggle">
+
+                <input type="checkbox" />
+                <span></span>
+                <span></span>
+                <span></span>
+
+
+
+                <ul id="menu">
+                 
+                    <li><a href="perfil.php">Perfil</a></li>
+                    <li><a href="ajuda.php">Ajuda</a></li>
+                    <li><a href="configuracoes.php">Configurações</a></li>
+                    <li><a href="amigos.php">Amigos</a></li>
+                    <li><a href="autores.php">Autores</a></li>
+                    <li><a href="sobre_nos.php">Sobre nós</a></li>
+                    <li><a href="sair.php">Sair</a></li>
+                </ul>
+
+
+            </div>
+        </nav>
+
+
+
+        <div class="s128">
+            <form method="post" action="itens.php">
+                <div class="inner-form">
+                    <div class="row">
+                        <div class="input-field first" id="first">
+
+                            <input class="input" id="inputFocus" type="text" placeholder="Pesquisar" name="pesquisar" />
+                            <input type="submit" name="enviar" id="pesqenviar">
+
+
+                            <button class="clear" id="clear">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                                    <path
+                                        d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z">
+                                    </path>
+                                </svg>
+                            </button>
+                        </div>
+                    </div>
+
+                </div>
+            </form>
+        </div>
+
+
+    </div>
 
 <main class="principal">
-<div class="formulario">
-        <div class="conteudo">
+<div class="container text-center"  style="border: 1px solid #191825;  background-color: #191825; width: 700px; height: 500px;
+    color: white;
+    margin-top: 100px;
+    border-radius: 4%">
+<div class="row">
+    <div class="col">
 
-<h1>Deletar conta</h1>
+<h1 class="delconta">Deletar conta</h1>
                 <form action="" method="POST">
                 
-                    <div class="modulo">
-                        <label for="logsenha">
-                            Email
+                    <div class="atnome">
+                        <label for="alt_nome">
+                            E-mail <br>
                             <input type="text" name="email">
                         </label>
                     </div>
-                   
-                    <div class="modulo">
-                        <label for="logsenha">
-                            Senha
-                            <input type="text" name="senha">
+                   <br>
+                       <div class="atnome">
+                        <label for="alt_nome">
+                            Senha <br>
+                            <input type="password" name="senha">
                         </label>
                     </div>
-                    <div class="modulo">
-                        <label for="logsenha">
-                            Confirme a senha
-                            <input type="text" name="csenha">
+                    <br>
+                    <div class="atnome">
+                        <label for="alt_nome">
+                            Confirme a senha <br>
+                            <input type="password" name="csenha">
                         </label>
-
+                     
                     </div>
-                    <div class="modulo">
-       
-     
-            
-                   </div>
-                   <input type="submit" name="confirmar">
+                    <br>
+                    <div class="atconfirmar">
+                   <input type="submit" name="confirmar" class="btn btn-danger">
+                    </div>
                 </form>
     
            
     </div>
     </div>
-    </main>
+</div>
+</main>
     <?php
 require_once "conexao/conexao.php";
 session_start();
