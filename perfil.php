f5ac3ea9c2f3b7b5c86f7a56272c5d426a2371fc
GPereira10 <143447877+GPereira10@users.noreply.github.com>
2023-12-08 09:21:51 -0300

    Update perfil.php

--- perfil.php
+++ perfil.php
@@ -273,7 +273,7 @@ if($resultado){
                 <div class="col-xs-6 col-md-3">
                     <h6>Links Rapidos</h6>
                     <ul class="footer-links">
-                        <li><a href="sobre_nos.php">Sobre nos</a></li>
+                        <li><a href="sobre_nos.php">Sobre nós</a></li>
                         <li><a href="#">Fale conosco</a></li>
                         <li><a href="politicas.html">Politica de Privacidade</a></li>
                         <li><a href="politicas.html">Termos</a></li>
