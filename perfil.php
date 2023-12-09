c2e199bb14a7f892de301d26c9629fee485f3d84
camilavsyu <147069206+camilavsyu@users.noreply.github.com>
2023-12-09 18:14:18 -0300

    Add files via upload

--- perfil.php
+++ perfil.php
@@ -2,73 +2,53 @@
 require_once "conexao/conexao.php";
 session_start();
 $usuario=$_SESSION["usuario"];
-$sql="SELECT * FROM usuario WHERE id_usuario = ".$usuario["id_usuario"]." ";
-$resultado=$conexao->query($sql);
-$id = $usuario ["id_usuario"];
+if($_SERVER["REQUEST_METHOD"]==="POST"){
+$usuario=$_SESSION["usuario"];
+$altnome=$_POST["nome"];
+$altsenha=$_POST["senha"];
+$email=$usuario["email"];
 
-if ($resultado && $resultado->num_rows > 0) {
-/*     echo "login efetuado com sucesso";
- */    
-    $dados = mysqli_fetch_array($resultado);
-    
-    $_SESSION["usuario"]=$dados;
 
-}
-    
-if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST["nome"])) {
-
-    
-    $altnome = $_POST["nome"];
-    $altsenha = $_POST["senha"];
-
-    // Certifique-se de que as variáveis POST existem antes de usá-las.
-    if (isset($altnome, $altsenha)) {
-     
-        
-        // Prepare a consulta SQL e vincule os parâmetros.
-        $sql="UPDATE `usuario`
+$sql="UPDATE `usuario`
 SET `nome_usuario`='$altnome' ,
 `senha`='$altsenha'
-WHERE `id_usuario`= '$id'";
-$resultado=$conexao->query($sql);
-if($resultado){
+WHERE `email`= '$email'";
+
+if($conexao->query($sql)){
     echo "<script language='javascript' type='text/javascript'>alert('Dados alterados com sucesso!')
     ;</script>";
-
-}else{
-
-}
-       /*  $stmt = $conexao->prepare($sql);
-        $stmt->bind_param("sss", $altnome, $altsenha, $email);
-
-        // Execute a consulta.
-        if ($stmt->execute()) {
-            echo "Dados atualizados com sucesso.";
-        } else {
-            echo "Erro ao atualizar os dados: " . $conexao->error;
-        } */
-
-    
+    function verificaLogin($email, $conexao)
+    {
+        $email = $conexao->real_escape_string($email);
+        $sql = "SELECT * FROM usuario WHERE email ='$email'";
+        $resultado = $conexao->query($sql);
+        return $resultado;
     }
+    $resultado = verificaLogin($email, $conexao);
+    $dados = mysqli_fetch_array($resultado);
+    $usuario = [
+        'nome_usuario' => $dados['nome_usuario'],
+        'email' => $dados['email'],
+        'senha' => $dados['senha'],
+        
+        'recuperacao' => $dados['recuperacao']
+    ];
+    $mens= "Olá: " .  $usuario["nome_usuario"] ."<br>"."Email: ". $usuario["email"];
+}else{
+    echo "<script language='javascript' type='text/javascript'>alert('Não foi possível altera')
+    ;</script>";
+
+}
 }
 
-    
-    
- 
-
-
-
-?> 
+    ?> 
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Perfil</title>
-    <link rel="shortcut icon" href="imagens/logo_projeto2.png">
     <link rel="stylesheet" href="assets/css/estilo.css">
- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
 </head>
 <body>
 <div class="cabecalho">
@@ -82,20 +62,16 @@ if($resultado){
 
 
 
-            
                 <ul id="menu">
-                <h2><a href="perfil.php"><i class="bi bi-person-circle"> </i>
-                    <?= $usuario['nome_usuario'] ?></a>
-                    </h2>                    <li><a href="inicial.php">Inicial</a></li>
-                    <li><a href="comunidade.php">Comunidade</a></li>
-                    <li><a href="Amigos.php">Amigos</a></li>
-                    <li><a href="carrinho.php">Carrinho</a></li>
+                    <h2>Usuário: <?= $usuario['nome_usuario']?> </h2>
+                    <li><a href="perfil.php">Perfil</a></li>
+                    <li><a href="ajuda.php">Ajuda</a></li>
+                    <li><a href="configuracoes.php">Configurações</a></li>
+                    <li><a href="amigos.php">Amigos</a></li>
                     <li><a href="autores.php">Autores</a></li>
-                    <li><a href="https://mail.google.com/mail/u/0/?fs=1&tf=cm&source=mailto&to=creatorsofthought@gmail.com">Ajuda</a></li>
                     <li><a href="sobre_nos.php">Sobre nós</a></li>
                     <li><a href="sair.php">Sair</a></li>
                 </ul>
-
 
 
             </div>
@@ -103,13 +79,12 @@ if($resultado){
 
 
 
-      <div class="s128">
-        <form method="post" action="pessoas.php">
+        <div class="s128">
+            <form>
                 <div class="inner-form">
                     <div class="row">
                         <div class="input-field first" id="first">
-                            <input class="input" id="inputFocus" type="text" placeholder="Pesquisar" name="pesquisarpessoa" />
-                            <input type="submit" name="enviar" id="pesqenviar">
+                            <input class="input" id="inputFocus" type="text" placeholder="Keyword" />
                             <button class="clear" id="clear">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
                                     <path
@@ -119,192 +94,27 @@ if($resultado){
                             </button>
                         </div>
                     </div>
-
+                    
                 </div>
             </form>
         </div>
-        </div>
-<div class="principal">
-<div class="container text-center " style="border: 1px solid #2f2841; border-radius: 3%; margin-top: 8%; height: 500px; ">
-<div class="row">
-    <div class="col   p-4">
-<form action="" method="post">
-    <h2 class="alterar">EDITAR IMAGEM</h2>
-    <input type="text" placeholder="Endereço da imagem" name="img" class="btn btn-light"/>
-    <input type="submit" name="enviarimg" id="enviarimg" placeholder="Endereço da imagem..."  class="btn btn-primary" style="font-weight: bold;" >
-    <?php
-    if (isset($_POST["img"])) {
-        $img = $_POST["img"];
-        $sql="UPDATE `usuario`
-        SET `img_perfil`='$img'
-        WHERE `id_usuario`= '$id'";
- 
-$resultado=$conexao->query($sql);
-if($resultado){
-    echo"<script language='javascript' type='text/javascript'>alert('Imagem alterada')
-    ;window.location.href='perfil.php'</script>"; 
-}else{
- echo "Não foi possivel atualizar a imagem";   
-}
-    }
-?>
-</form>  
-            
-        <?php
-    if($dados["img_perfil"]){    
-    ?>
-    <img class="profile-pic" id="iconperfil" src="<?=$dados["img_perfil"]?>" >
-    
-    <?php 
-    }else{ 
-        ?>
-       <img class="profile-pic" id="iconperfil" src="https://img.freepik.com/fotos-gratis/icone-de-perfil-de-usuario-frontal-com-fundo-branco_187299-40010.jpg?w=740&t=st=1697032016~exp=1697032616~hmac=d6f954d7e8c6ce2127a1fc24d262b5a7ccaa9abffb3a117fe93d5bc3055b5ab0">
-       
-    <?php
-    }
-    ?>
 
-  
-    <h4 class="n_usuario"><?= $dados["nome_usuario"]?></h4>
-    <br>
-  <h2 class="alterar">BIO </h2>
-  <?php 
 
-if ($dados["biografia"]) {
-    if (isset($_POST["alterarbio"])) {
-        $bio = $_POST["bio2"];
-        $sql="UPDATE `usuario`
-        SET `biografia`='$bio'
-        WHERE `id_usuario`= '$id'";
-/*  $sql="INSERT biografia INTO usuario VALUE ($bio); "        ;
- */    
-$resultado=$conexao->query($sql);
-
-if($resultado){
-    header("location: perfil.php");
-}else{
- echo "Não foi possivel atualizar a bio";   
-}
-    }
-    echo "<p style=' font-size: 20px;'>" . $dados["biografia"] . "</p>";
-    ?>
-    <form action="" method="post">
-        <input type="text" placeholder="Edite sua bio!" name="bio2" class="btn btn-light" />
-        <input type="submit" name="alterarbio" id="envbio"  class="btn btn-primary" style="font-weight: bold;">
-    </form>
-    <?php
-} else {
-    if (isset($_POST["enviarbio"])) {
-        $bio = $_POST["bio"];
-        $sql="UPDATE `usuario`
-        SET `biografia`='$bio'
-        
-        WHERE `id_usuario`= '$id'";
-/*  $sql="INSERT biografia INTO usuario VALUE ($bio); "        ;
- */    
-$resultado=$conexao->query($sql);
-header("location: perfil.php");
-exit();
-if($resultado){
-    echo $bio;
-}else{
- echo "Não foi possivel atualizar a bio";   
-}
-   
-        
-    } else {
-        ?>
-        <form action="" method="post">
-            <input type="text" placeholder="Insira algo aqui..." name="bio" class="btn btn-light" />
-            <input type="submit" name="enviarbio" id="envbio"  class="btn btn-primary" style="font-weight: bold;">
-        </form>
-        <?php
-    }
-}
-?>
-</div>
-    <div class="col  p-4" style="background-color:#2f2841; color: white; border-radius: 3%;  height: 500px; ">
+    </div>
     <form action="" method="POST">
-            <h1> <?=     $mens?> </h1>
-
-            <h2 class="alterar" style=" color: white;"> ALTERAR INFORMAÇÕES</h2>
+            <h2> <?=     $mens?> </h2>
+            <h1> ALTERAR INFORMAÇÕES</h1>
             <br>
-            <div class="atnome">
-           <label for="alt_nome"> Nome <br>
+            Nome
             <input type="text" name="nome">
-        </label>
-</div>
-<br>
-<div class="atnome">
-            <label for="alt_senha">Senha <br>
-            <input type="text" name="senha"> 
-</label>
-<br>  <br>
-</div>
-<div class="atenviar">
-<label for="alt_enviar">
-            <input type="submit" name="enviar" class="envio">
-</label>
-</div>
+            Senha
+            <input type="text" name="senha">
+            <input type="submit" name="enviar">
+
         </form>
-        <a href="deletarconta"><input type="submit" name="delet" value="Deletar conta" class="btn btn-danger" style="font-weight: bold;"></a>
+
+        <a href="deletarconta"><input type="submit" name="delet" value="deletarconta"></a>
         </form>
-        
-</div>
-</div>
-</div>
-</div>
-</div>
-<!-- Site footer -->
- <footer class="site-footer mt-5 ">
-        <div class="container">
-            <div class="row">
-                <div class="col-sm-12 col-md-6">
-                    <h6>Sobre</h6>
-                    <p class="text-justify">O desenvolvimento deste site se tornou necessário após uma breve
-                        pesquisa
-                        sobre sites com o mesmo propósito, contudo, percebemos que estes sites são quase
-                        inexistentes.
-                        Visando isso, decidimos fazer um site com mais reconhecimento para autores nacionais e para
-                        que
-                        mais pessoas possam ter gosto pela leitura.</p>
-                </div>
-
-                <div class="col-xs-6 col-md-3">
-                    <h6>Links Rapidos</h6>
-                    <ul class="footer-links">
-                        <li><a href="sobre_nos.php">Sobre nós</a></li>
-                        <li><a href="#">Fale conosco</a></li>
-                        <li><a href="politicas.html">Politica de Privacidade</a></li>
-                        <li><a href="politicas.html">Termos</a></li>
-                    </ul>
-                </div>
-            </div>
-            <hr>
-        </div>
-        <div class="container">
-            <div class="row">
-                <div class="col-md-8 col-sm-6 col-xs-12">
-
-                </div>
-
-                <div class="col-md-4 col-sm-6 col-xs-12">
-                    <ul class="social-icons">
-                        <li><a class="facebook" href="#"><i class="bi bi-facebook"></i></a></li>
-                        <li><a class="twitter" href="#"><i class="bi bi-twitter"></i></a></li>
-                        <li><a class="insta" href="https://instagram.com/creatorsofthought?igshid=MzRlODBiNWFlZA"><i
-                                    class="bi bi-instagram"></i></a></li>
-                        <li><a class="linkedin" href="#"><i class="bi bi-linkedin"></i></a></li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-    </footer>
-
-
-  
-        
-
  <script src="js/menulateral.js"></script>
 
 </body>
